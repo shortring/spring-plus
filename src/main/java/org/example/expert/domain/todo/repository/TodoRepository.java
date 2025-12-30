4032674284280927ea5f2605ac628a20e7c9665a
chan <zinchan1@naver.com>
2025-12-30 12:53:11 +0900

    ✨ feat: LV1-3 : weather검색 조건 추가
    jpql적용

--- src/main/java/org/example/expert/domain/todo/repository/TodoRepository.java
+++ src/main/java/org/example/expert/domain/todo/repository/TodoRepository.java
@@ -1,6 +1,9 @@
 package org.example.expert.domain.todo.repository;
 
+import com.querydsl.jpa.impl.JPAQueryFactory;
 import org.example.expert.domain.todo.entity.Todo;
+import org.example.expert.domain.user.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -9,13 +12,19 @@ import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
 
-public interface TodoRepository extends JpaRepository<Todo, Long> {
+public interface TodoRepository extends JpaRepository<Todo, Long>, QTodoRepository {
 
-    @Query("SELECT t FROM Todo t LEFT JOIN FETCH t.user u ORDER BY t.modifiedAt DESC")
-    Page<Todo> findAllByOrderByModifiedAtDesc(Pageable pageable);
+    @Query("""
+            SELECT t FROM Todo t 
+            LEFT JOIN FETCH t.user u
+            WHERE :weather is null or t.weather = :weather
+            ORDER BY t.modifiedAt DESC
+            """)
+    Page<Todo> findAllByOrderByModifiedAtDescAndWeather(Pageable pageable, String weather);
 
-    @Query("SELECT t FROM Todo t " +
-            "LEFT JOIN t.user " +
-            "WHERE t.id = :todoId")
-    Optional<Todo> findByIdWithUser(@Param("todoId") Long todoId);
+//    @Query("SELECT t FROM Todo t " +
+//            "LEFT JOIN t.user " +
+//            "WHERE t.id = :todoId")
+//    Optional<Todo> findByIdWithUser(@Param("todoId") Long todoId);
+
 }
